6bb714777128e1f8cd9c053429611fe8e820099b
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2022-04-14 04:58:23 +0900

    .
    로그인페이지 css 조절

--- react/final_v4_test/src/routes/Login.js
+++ react/final_v4_test/src/routes/Login.js
@@ -1,52 +1,45 @@
-import React from "react";
-import {Link, Redirect} from "react-router-dom";
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
 const Login = () => {
   return (
-      <div id="wrapper">
-          <div id="login" className="mx-auto my-5">
-            <aside className="">
-              <figure>
-                <Link to="/coverPage" className="logo_account">
+    <div id="wrapper">
+      <div className="container px-4">
+        <div id="login" className="mx-auto my-5 px-4">
+            <figure>
+              <Link to="/coverPage" className="logo_account">
+                <img src="img/popcon_logo_test.png" alt="" width="181.35" height="35" className="dark" />
+              </Link>
+            </figure>
+
+            <form className=" mx-auto mx-5">
+              <div className="access_social">
+                <Link to="/" className="social_bt kakao">
                   <img
-                      src="img/popcon_logo_test.png"
-                      alt=""
-                      width="181.35"
-                      height="35"
-                      className="dark"
-                  />
+                    className="kakao_logo col-2"
+                    src="img/KakaoTalk_logo.png"
+                    style={{ height: '20px', width: '20px' }}
+                  />{' '}
+                  카카오로 시작하기
                 </Link>
-              </figure>
-
-              <form className=" mx-auto mx-5 px-5">
-                  <div className="access_social">
-
-                    <Link to="/" className="social_bt kakao">
-                      <img
-                          className="kakao_logo col-2"
-                          src="img/KakaoTalk_logo.png"
-                          style={{height: '20px', width: '20px'}}
-                      />{" "}
-                      카카오로 시작하기
-                    </Link>
-                  </div>
-                <div className="text-center add_top_10">
-                  팝콘은 처음이신가요?{" "}
-                  <strong>
-                    <Link to="/#0">카카오로 시작해보세요!</Link>
-                  </strong>
-                </div>
-              </form>
-              <div className="copy">© 2022 PopCon</div>
-            </aside>
-          </div>
-          {/*/login */}
-
-          {/*COMMON SCRIPTS */}
-          <script src="js/common_scripts.min.js"></script>
-          <script src="js/bootstrap.bundle.min.js"></script>
-          <script src="js/common_func.js"></script>
+              </div>
+              <div className="text-center add_top_10">
+                팝콘은 처음이신가요?{' '}
+                <strong>
+                  <Link to="/#0">카카오로 시작해보세요!</Link>
+                </strong>
+              </div>
+            </form>
+            <div className="copy">© 2022 PopCon</div>
+        </div>
       </div>
+      {/*/login */}
+
+      {/*COMMON SCRIPTS */}
+      <script src="js/common_scripts.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/common_func.js"></script>
+    </div>
   );
 };
 
