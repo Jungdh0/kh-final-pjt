b6436811ac9106fff98b2d1d8f567ff123aa94c5
Joy <63247146+joyoo1221@users.noreply.github.com>
2022-04-15 17:45:22 +0900

    ott로고, 관람등급, 샘플데이터

--- react/final_v4_test/src/routes/MainPage.js
+++ react/final_v4_test/src/routes/MainPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import movies from '../sampledata/OTT별 작품 - 샘플 데이터.json';
+import movies from '../sampledata/샘플.json';
 
 const MainPage = () => {
   return (
@@ -82,9 +82,10 @@ const MainPage = () => {
                       </div>
                       <div>
                         <h6>{movie.content_name}</h6>
-                        <span>
-                          <img src={movie.ott_code}width="20px" alt="" />
-                        </span>
+                        <figure>
+                          <img src={movie.ott_code} width="20px" alt="" />
+                          <img src={movie.age_rating} width="20px" alt="" />
+                        </figure>
                       </div>
                     </Link>
                   </div>
