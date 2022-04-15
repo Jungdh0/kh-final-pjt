8f754e9d60c38195871c7d8fa8152fa71d5182b3
Joy <63247146+joyoo1221@users.noreply.github.com>
2022-04-15 22:09:25 +0900

    디테일 수정

--- react/final_v4_test/src/routes/DetailPage.js
+++ react/final_v4_test/src/routes/DetailPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import movies from "../sampledata/샘플.json";
+import {Link} from "react-router-dom";
 
 const DetailPage = () => {
     return (
@@ -255,143 +257,67 @@ const DetailPage = () => {
                 </div>
 
                 <div className="container margin_60_40_second" style={{marginTop: 20}}>
-                    <h4>비슷한 콘텐츠</h4>
                     <hr/>
-                    <div className="row">
-                        <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                            <div className="strip">
-                                <figure>
-                                    <img
-                                        src="https://images.justwatch.com/poster/262548358/s592/sanae-majseon"
-                                        data-src="https://images.justwatch.com/poster/262548358/s592/sanae-majseon"
-                                        className="lazy"
-                                        alt=""
-                                        width="533"
-                                        height="400"
-                                    />
-                                    <a href="detail-page.html" className="strip_info">
-                                        <div className="item_title">
-                                            <h3>사내 맞선</h3>
-                                        </div>
-                                    </a>
-                                </figure>
-                                <ul>
-                                    <li>
-                                        <a href="author.html" className="author">
-                                            <div className="author_thumb veryfied">
-                                                <figure>
-                                                    <img
-                                                        src="https://velog.velcdn.com/images/joyoo1221/post/1a7edb63-cffc-4401-af47-06bc3e6b742c/image.png"
-                                                        data-src="https://velog.velcdn.com/images/joyoo1221/post/1a7edb63-cffc-4401-af47-06bc3e6b742c/image.png"
-                                                        alt=""
-                                                        className="lazy"
-                                                        width="100"
-                                                        height="100"
-                                                    />
-                                                </figure>
-                                            </div>
-                                            <h6>Tving</h6>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#0" className="wish_bt"
-                                        ><i className="bi bi-heart-fill"></i
-                                        ></a>
-                                        129
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                            <div className="strip">
-                                <figure>
-                                    <img
-                                        src="img/items/item-1-placeholder.png"
-                                        data-src="img/poster/오징어게임.jpeg"
-                                        className="lazy"
-                                        alt=""
-                                        width="533"
-                                        height="400"
-                                    />
-                                    <a href="detail-page.html" className="strip_info">
-                                        <div className="item_title">
-                                            <h3>오징어게임</h3>
-                                        </div>
-                                    </a>
-                                </figure>
-                                <ul>
-                                    <li>
-                                        <a href="author.html" className="author">
-                                            <div className="author_thumb">
-                                                <figure>
-                                                    <img
-                                                        src="https://velog.velcdn.com/images/joyoo1221/post/b807b710-c2e1-41a5-9175-b6607eac20d5/image.png"
-                                                        data-src="img/netflix_logo.jpeg"
-                                                        alt=""
-                                                        className="lazy"
-                                                        width="100"
-                                                        height="100"
-                                                    />
-                                                </figure>
-                                            </div>
-                                            <h6>Netflix</h6>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#0" className="wish_bt"
-                                        ><i className="bi bi-heart-fill"></i
-                                        ></a>
-                                        50
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                            <div className="strip">
-                                <figure>
-                                    <img
-                                        src="https://image.tving.com/upload/cms/caip/CAIP0900/P001565742.jpg/dims/resize/F_webp,400"
-                                        data-src="https://image.tving.com/upload/cms/caip/CAIP0900/P001565742.jpg/dims/resize/F_webp,400"
-                                        className="lazy"
-                                        alt=""
-                                        width="533"
-                                        height="400"
-                                    />
-                                    <a href="detail-page.html" className="strip_info">
-                                        <div className="item_title">
-                                            <h3>돼지의 왕</h3>
-                                        </div>
-                                    </a>
-                                </figure>
-                                <ul>
-                                    <li>
-                                        <a href="author.html" className="author">
-                                            <div className="author_thumb veryfied">
-                                                <figure>
-                                                    <img
-                                                        src="https://velog.velcdn.com/images/joyoo1221/post/1a7edb63-cffc-4401-af47-06bc3e6b742c/image.png"
-                                                        data-src="https://velog.velcdn.com/images/joyoo1221/post/1a7edb63-cffc-4401-af47-06bc3e6b742c/image.png"
-                                                        alt=""
-                                                        className="lazy"
-                                                        width="100"
-                                                        height="100"
-                                                    />
-                                                </figure>
-                                            </div>
-                                            <h6>Tving</h6>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#0" className="wish_bt"
-                                        ><i className="bi bi-heart-fill"></i
-                                        ></a>
-                                        159
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                    <div className="main_title version_2">
+                        <span>
+                          <em></em>
+                        </span>
+                        <h2>비슷한 콘텐츠</h2>
                     </div>
-
+                    {/*<hr/>*/}
+                    <div className="row">
+                        {movies.map((movie, i) => {
+                            if (i > 4) {
+                                return;
+                            }
+                            if (i) {
+                                return(
+                                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6">
+                                    <div className="strip">
+                                        <figure>
+                                            <img
+                                                src={movie.content_img_ver}
+                                                className="lazy"
+                                                alt=""
+                                                width="533"
+                                                height="400"
+                                            />
+                                            <a href="detail-page.html" className="strip_info">
+                                                <div className="item_title">
+                                                    <h3>{movie.content_name}</h3>
+                                                </div>
+                                            </a>
+                                        </figure>
+                                        <ul>
+                                            <li>
+                                                <Link to="/detailPage" className="author">
+                                                    <div className="author_thumb veryfied">
+                                                        <figure>
+                                                            <img
+                                                                src="https://velog.velcdn.com/images/joyoo1221/post/1a7edb63-cffc-4401-af47-06bc3e6b742c/image.png"
+                                                                alt=""
+                                                                className="lazy"
+                                                                width="100"
+                                                                height="100"
+                                                            />
+                                                        </figure>
+                                                    </div>
+                                                    <h6>Tving</h6>
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <a href="#0" className="wish_bt"
+                                                ><i className="bi bi-heart-fill"></i
+                                                ></a>
+                                                129
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                );
+                            }
+                        })}
+                    </div>
                     {/* /row */}
                 </div>
                 {/* /container */}
