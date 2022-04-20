1b66a56304ba917aede2eb4a742b08efe9006d00
milmil <65494214+milmilkim@users.noreply.github.com>
2022-04-20 19:26:38 +0900

    isLoading (#13)
    제곧내

--- react/final_v4_test/src/routes/DetailPage.js
+++ react/final_v4_test/src/routes/DetailPage.js
@@ -23,14 +23,17 @@ let tag = {
 
 const DetailPage = () => {
   const [movie, setMovie] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
   let { contentCode } = useParams();
   let settag = movie.tagName;
   useEffect(() => {
     (async () => {
       try {
+        setIsLoading(true);
         const res = await axios.get(`${BASE_URL}/movies/${contentCode}`);
         setMovie(res.data);
-        console.log(res);
+        setIsLoading(false);
       } catch (e) {
         console.error(e);
       }
@@ -41,153 +44,156 @@ const DetailPage = () => {
     <div>
       <main>
         <div className="container margin_60_40" id="wrapper">
-          <div className="row">
-            <div className="col-xl-7 col-lg-7" style={{ display: 'flex', justifyContent: 'center' }}>
-              <figure>
-                <img src={movie.contentImgVer} alt="" className="img-fluid rounded" height="700" width="400" />
-              </figure>
-            </div>
-            {/* /col */}
+          {isLoading ? (
+            <>loading...</>
+          ) : (
+            <div className="row">
+              <div className="col-xl-7 col-lg-7" style={{ display: 'flex', justifyContent: 'center' }}>
+                <figure>
+                  <img src={movie.contentImgVer} alt="" className="img-fluid rounded" height="700" width="400" />
+                </figure>
+              </div>
+              {/* /col */}
 
-            <div className="col-xl-5 col-lg-5">
-              <div className="detail_col">
-                <div className="main_info clearfix mb-1" style={{ display: 'flex' }}>
-                  <div className="item_desc">
-                    <div className="mb-3">
-                      <a href="author.html" className="author">
-                        <div className="author_thumb veryfied" style={{ display: 'flex' }}>
-                          <figure style={{ height: 30, width: 30 }}>
-                            <img src={movie.ottImg} style={{ objectFit: 'cover' }} alt={movie.ottName} className="lazy loaded" data-was-processed="true" />
-                          </figure>
-                        </div>
-                      </a>
+              <div className="col-xl-5 col-lg-5">
+                <div className="detail_col">
+                  <div className="main_info clearfix mb-1" style={{ display: 'flex' }}>
+                    <div className="item_desc">
+                      <div className="mb-3">
+                        <a href="author.html" className="author">
+                          <div className="author_thumb veryfied" style={{ display: 'flex' }}>
+                            <figure style={{ height: 30, width: 30 }}>
+                              <img src={movie.ottImg} style={{ objectFit: 'cover' }} alt={movie.ottName} className="lazy loaded" data-was-processed="true" />
+                            </figure>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                    <div className="age">
+                      <figure>
+                        <img src={movie.ageRating} style={{ width: 30, height: 30, objectFit: 'cover' }} />
+                      </figure>
                     </div>
                   </div>
-                  <div className="age">
-                    <figure>
-                      <img src={movie.ageRating} style={{ width: 30, height: 30, objectFit: 'cover' }} />
-                    </figure>
-                  </div>
-                </div>
-                <hr />
-                <h1 className="mb-md-2">{movie.contentName}</h1>
-                <p>{movie.contentPlot}</p>
-                <br />
-                <br />
-                {/* <p className="item_price clearfix" style={{ display: 'flex', justifyContent: 'end' }}>
+                  <hr />
+                  <h1 className="mb-md-2">{movie.contentName}</h1>
+                  <p>{movie.contentPlot}</p>
+                  <br />
+                  <br />
+                  {/* <p className="item_price clearfix" style={{ display: 'flex', justifyContent: 'end' }}>
                   <a href="" className="btn_1 modal_popup">
                     보러 가기
                   </a>
                 </p> */}
 
-                <div className="tabs_detail">
-                  <ul className="nav nav-tabs" role="tablist">
-                    <li className="nav-item">
-                      <a id="tab-A" href="#pane-A" className="nav-link active" data-bs-toggle="tab" role="tab">
-                        서비스
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a id="tab-B" href="#pane-B" className="nav-link" data-bs-toggle="tab" role="tab">
-                        개봉년도
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a id="tab-C" href="#pane-C" className="nav-link" data-bs-toggle="tab" role="tab">
-                        정보
-                      </a>
-                    </li>
-                  </ul>
-                  <div className="tab-content" role="tablist">
-                    <div id="pane-A" className="card tab-pane fade show active" role="tabpanel" aria-labelledby="tab-A">
-                      <div className="card-header" role="tab" id="heading-A">
-                        <h5>
-                          <a className="collapsed" data-bs-toggle="collapse" href="#collapse-A">
-                            서비스
-                          </a>
-                        </h5>
-                      </div>
-                      <div id="collapse-A" className="collapse" role="tabpanel" aria-labelledby="heading-A">
-                        <div className="pt-4" id="service_ott">
-                          <div className="author_list">
-                            <a href="author.html" className="author">
-                              <div className="author_thumb veryfied">
-                                <img src={movie.ottImg} alt="" className="lazy" style={{ width: 60, height: 60, objectFit: 'cover' }} />
-                              </div>
-                              <div>
-                                <h6>{movie.ottName}</h6>
-                              </div>
+                  <div className="tabs_detail">
+                    <ul className="nav nav-tabs" role="tablist">
+                      <li className="nav-item">
+                        <a id="tab-A" href="#pane-A" className="nav-link active" data-bs-toggle="tab" role="tab">
+                          서비스
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a id="tab-B" href="#pane-B" className="nav-link" data-bs-toggle="tab" role="tab">
+                          개봉년도
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a id="tab-C" href="#pane-C" className="nav-link" data-bs-toggle="tab" role="tab">
+                          정보
+                        </a>
+                      </li>
+                    </ul>
+                    <div className="tab-content" role="tablist">
+                      <div id="pane-A" className="card tab-pane fade show active" role="tabpanel" aria-labelledby="tab-A">
+                        <div className="card-header" role="tab" id="heading-A">
+                          <h5>
+                            <a className="collapsed" data-bs-toggle="collapse" href="#collapse-A">
+                              서비스
                             </a>
-                          </div>
+                          </h5>
                         </div>
-                      </div>
-                    </div>
-                    {/* /tab */}
-                    <div id="pane-B" className="card tab-pane fade" role="tabpanel" aria-labelledby="tab-B">
-                      <div className="card-header" role="tab" id="heading-B">
-                        <h5>
-                          <a className="collapsed" data-bs-toggle="collapse" href="#collapse-B">
-                            개봉년도
-                          </a>
-                        </h5>
-                      </div>
-                      <div id="collapse-B" className="collapse" role="tabpanel" aria-labelledby="heading-B">
-                        <div className="pt-4">
-                          <div className="row author_list">
-                            <div className="col-lg-12 pt-5">
-                              <a href="" className="author">
-                                <div className="author_thumb veryfied"></div>
+                        <div id="collapse-A" className="collapse" role="tabpanel" aria-labelledby="heading-A">
+                          <div className="pt-4" id="service_ott">
+                            <div className="author_list">
+                              <a href="author.html" className="author">
+                                <div className="author_thumb veryfied">
+                                  <img src={movie.ottImg} alt="" className="lazy" style={{ width: 60, height: 60, objectFit: 'cover' }} />
+                                </div>
                                 <div>
-                                  <span>{movie.releaseYear}</span>
+                                  <h6>{movie.ottName}</h6>
                                 </div>
                               </a>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                    {/* /tab */}
-                    <div id="pane-C" className="card tab-pane fade" role="tabpanel" aria-labelledby="tab-C">
-                      <div className="card-header" role="tab" id="heading-C">
-                        <h5>
-                          <a className="collapsed" data-bs-toggle="collapse" href="#collapse-C">
-                            정보
-                          </a>
-                        </h5>
-                      </div>
-                      <div id="collapse-C" className="collapse" role="tabpanel" aria-labelledby="heading-C">
-                        <div className="pt-4">
-                          <div className="row">
-                            <div className="col-lg-12 pt-5">
-                              <ul className="bullets">
-                                <li>
-                                  감독 <span>{movie.director}</span>
-                                </li>
-                                <li>
-                                  출연
-                                  <span>{movie.actor}</span>
-                                </li>
-                                <li>
-                                  태그
-                                  {/* // key값이 settag인 자료를 뽑아 */}
-                                  {tag[settag]}
-                                </li>
-                              </ul>
+                      {/* /tab */}
+                      <div id="pane-B" className="card tab-pane fade" role="tabpanel" aria-labelledby="tab-B">
+                        <div className="card-header" role="tab" id="heading-B">
+                          <h5>
+                            <a className="collapsed" data-bs-toggle="collapse" href="#collapse-B">
+                              개봉년도
+                            </a>
+                          </h5>
+                        </div>
+                        <div id="collapse-B" className="collapse" role="tabpanel" aria-labelledby="heading-B">
+                          <div className="pt-4">
+                            <div className="row author_list">
+                              <div className="col-lg-12 pt-5">
+                                <a href="" className="author">
+                                  <div className="author_thumb veryfied"></div>
+                                  <div>
+                                    <span>{movie.releaseYear}</span>
+                                  </div>
+                                </a>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
+                      {/* /tab */}
+                      <div id="pane-C" className="card tab-pane fade" role="tabpanel" aria-labelledby="tab-C">
+                        <div className="card-header" role="tab" id="heading-C">
+                          <h5>
+                            <a className="collapsed" data-bs-toggle="collapse" href="#collapse-C">
+                              정보
+                            </a>
+                          </h5>
+                        </div>
+                        <div id="collapse-C" className="collapse" role="tabpanel" aria-labelledby="heading-C">
+                          <div className="pt-4">
+                            <div className="row">
+                              <div className="col-lg-12 pt-5">
+                                <ul className="bullets">
+                                  <li>
+                                    감독 <span>{movie.director}</span>
+                                  </li>
+                                  <li>
+                                    출연
+                                    <span>{movie.actor}</span>
+                                  </li>
+                                  <li>
+                                    태그
+                                    {/* // key값이 settag인 자료를 뽑아 */}
+                                    {tag[settag]}
+                                  </li>
+                                </ul>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      {/* /tab */}
                     </div>
-                    {/* /tab */}
+                    {/* /tab-content */}
                   </div>
-                  {/* /tab-content */}
+                  {/* /tabs_detail */}
                 </div>
-                {/* /tabs_detail */}
+                {/* /detail_col */}
               </div>
-              {/* /detail_col */}
-            </div>
 
-            {/* <div className="container margin_60_40_second" style={{ marginTop: 20 }}>
+              {/* <div className="container margin_60_40_second" style={{ marginTop: 20 }}>
               <hr />
               <div className="main_title version_2">
                 <span>
@@ -237,9 +243,9 @@ const DetailPage = () => {
                 })}
               </div>
             </div> */}
-            {/* /col */}
-          </div>
-          {/* /row */}
+              {/* /col */}
+            </div>
+          )}
         </div>
       </main>
 
