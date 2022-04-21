e78cb110580bb1f1dd3d3ea7d22dd6195924aaaa
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2022-04-21 14:32:53 +0900

    태그 버튼형으로 바꿈

--- react/final_v4_test/src/routes/ExplorePage.js
+++ react/final_v4_test/src/routes/ExplorePage.js
@@ -132,6 +132,7 @@ const ExplorePage = () => {
             </div>
           </div>
         </div>
+
         {/* /filters_full */}
         <div className="container margin_30_40">
           <div className="page_header">
@@ -151,46 +152,46 @@ const ExplorePage = () => {
             <nav className="sausage-links">
               <ul>
                 <li>
-                  <a href="">#맛깔나는</a>
+                  <button>#맛깔나는</button>
                 </li>
                 <li>
-                  <a href="">#퀸카가되고싶어?</a>
+                  <button>#퀸카가되고싶어?</button>
                 </li>
                 <li>
-                  <a href="">#방구석여행</a>
+                  <button>#방구석여행</button>
                 </li>
                 <li>
-                  <a href="">#미친상상력의비밀</a>
+                  <button>#미친상상력의비밀</button>
                 </li>
                 <li>
-                  <a href="">#냠냠쩝쩝</a>
+                  <button>#냠냠쩝쩝</button>
                 </li>
                 <li>
-                  <a href="">#잔혹한</a>
+                  <button>#잔혹한</button>
                 </li>
                 <li>
-                  <a href="">#심장마비오는</a>
+                  <button>#심장마비오는</button>
                 </li>
                 <li>
-                  <a href="">#브금맛집</a>
+                  <button>#브금맛집</button>
                 </li>
                 <li>
-                  <a href="">#빨간맛궁금해허니</a>
+                  <button>#빨간맛궁금해허니</button>
                 </li>
                 <li>
-                  <a href="">#감정을파고드는</a>
+                  <button>#감정을파고드는</button>
                 </li>
                 <li>
-                  <a href="">#폰할머니집</a>
+                  <button>#폰할머니집</button>
                 </li>
                 <li>
-                  <a href="">#당신의기숙사는?</a>
+                  <button>#당신의기숙사는?</button>
                 </li>
                 <li>
-                  <a href="">#흥미진진한</a>
+                  <button>#흥미진진한</button>
                 </li>
                 <li>
-                  <a href="">#아이들과보기좋은</a>
+                  <button>#아이들과보기좋은</button>
                 </li>
               </ul>
             </nav>
