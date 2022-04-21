0630a89f4cc6ec985d70f9a76ed384429603f71f
Joy <63247146+joyoo1221@users.noreply.github.com>
2022-04-21 10:49:29 +0900

    -

--- react/final_v4_test/src/components/App.js
+++ react/final_v4_test/src/components/App.js
@@ -1,10 +1,6 @@
-import React from "react";
-import AppRouter from "./Router";
-import { Route, Switch } from "react-router-dom";
-import Login from "../routes/Login";
-import CoverPage from "../routes/CoverPage";
-import TestPage from "../routes/TestPage";
-import { RecoilRoot } from "recoil";
+import React from 'react';
+import AppRouter from './Router';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
