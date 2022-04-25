1ad697a9010d001b9a2466dcec38570f671d3829
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2022-04-25 18:52:47 +0900

    test Result>link to 추가

--- react/final_v4_test/src/components/test/Result.js
+++ react/final_v4_test/src/components/test/Result.js
@@ -1,5 +1,6 @@
-import infoList from "../../sampledata/infoList"; //결과
-import React from "react";
+import infoList from '../../sampledata/infoList'; //결과
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Result = ({ resultVisible, visible, hidden, select }) => {
   console.log(select);
@@ -7,51 +8,49 @@ const Result = ({ resultVisible, visible, hidden, select }) => {
 
   const point = select.indexOf(Math.max(...select));
 
-  const resultsURL = "img/result/results/";
-  const leftURL = resultsURL + point + "-A.jpg";
-  const centerURL = resultsURL + point + "-B.jpg";
-  const rightURL = resultsURL + point + "-C.jpg";
+  const resultsURL = 'img/result/results/';
+  const leftURL = resultsURL + point + '-A.jpg';
+  const centerURL = resultsURL + point + '-B.jpg';
+  const rightURL = resultsURL + point + '-C.jpg';
 
   console.log(resultVisible);
   return (
     <>
-      <section
-        id="result"
-        class="mx-auto mt-5 py-5 px-3"
-        style={resultVisible ? visible : hidden}
-      >
+      <section id="result" class="mx-auto mt-5 py-5 px-3" style={resultVisible ? visible : hidden}>
         <div id="resultImg" class="col-lg-6 col-md-8 col-sm-10 mx-auto"></div>
         <div class="px-3 mt-3 text-center">
           <h3 class="py-3">당신의 유형은?</h3>
           <span class="resultname highlight">{infoList[point].name}</span>
-          <div class="resultDesc col-lg-6 col-md-8 col-sm-10 mx-auto py-3">
-            {infoList[point].desc}
-          </div>
+          <div class="resultDesc col-lg-6 col-md-8 col-sm-10 mx-auto py-3">{infoList[point].desc}</div>
         </div>
         <div class="row" id="resultImgs">
           <div class="col-sm-4">
-            <img src={leftURL} class="leftImage" alt={point} width="100%" />
+            <img src={leftURL} class="leftImage" alt={point} width="100%" style={{ borderRadius: '3px' }} />
           </div>
           <div class="col-sm-4">
-            <img src={centerURL} class="centerImage" alt={point} width="100%" />
+            <img src={centerURL} class="centerImage" alt={point} width="100%" style={{ borderRadius: '3px' }} />
           </div>
           <div class="col-sm-4">
-            <img src={rightURL} class="rightImage" alt={point} width="100%" />
+            <img src={rightURL} class="rightImage" alt={point} width="100%" style={{ borderRadius: '3px' }} />
           </div>
         </div>
-        <div>
-          <button class="default-btn mt-3">더 많은 추천 보러가기</button>
+        <div style={{ marginTop: '30px' }}>
+          <Link
+            to="/login"
+            class="default-btn mt-5
+          "
+          >
+            더 많은 추천 보러가기
+          </Link>
         </div>
-        <div>
-          <button type="button" class="btn btn-warning mt-3 mx-auto">
+        {/* <Link to="/login" type="button" class="btn btn-warning mt-3 mx-auto">
             {/* <img
               src="img/KakaoTalk_logo.png"
               style="width: 30px;"
               class="px-1"
             /> */}
-            공유하기
-          </button>
-        </div>
+        {/* 공유하기 */}
+        {/* </Link> */}
       </section>
     </>
   );
