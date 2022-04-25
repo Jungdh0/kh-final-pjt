3bfba9f6752c14172830ae1630473dd1460de177
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2022-04-25 21:57:08 +0900

    캐러셀>버튼
    피드백 대로 보라색으로 바꿔봤으나 구려서..ㅠ원복하고 border style 들어간거 뺐어요

--- react/final_v4_test/src/components/Slider/SlideButton.js
+++ react/final_v4_test/src/components/Slider/SlideButton.js
@@ -3,7 +3,7 @@ import { ReactComponent as ArrowIcon } from '../../assets/icons/ic_arrow.svg';
 export default function SlideButton({ direction, onClick }) {
   return (
     <button onClick={onClick} className={`btn-slide-control btn-${direction}`}>
-      <ArrowIcon width="16" height="16" fill="#7e2ad1" />
+      <ArrowIcon width="16" height="16" fill="#727272" />
     </button>
   );
 }
