d7bd062f05ad1c8207fc59a968a909a0b2f6e1a8
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2022-04-25 20:38:08 +0900

    test result 수정

--- react/final_v4_test/src/routes/Auth.js
+++ react/final_v4_test/src/routes/Auth.js
@@ -27,19 +27,43 @@ const Auth = () => {
 
   //코드를 백엔드로 보내서 토큰 받아와야 됨~~!!
   return (
-    <div class="cssload-container">
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
-      <div class="cssload-thing"></div>
+    <div class="kart-loader">
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
+      <div class="sheath">
+        <div class="segment"></div>
+      </div>
     </div>
   );
 };
--- react/final_v4_test/src/sampledata/infoList.js
+++ react/final_v4_test/src/sampledata/infoList.js
@@ -29,31 +29,31 @@ const infoList = [
   },
   {
     name: '#빨간맛궁금해허니',
-    desc: "I do the same thing I told you that I never would I told you I changed, even when I knew I never could I know that I can't find nobody else as good as you I need you to stay, need you to stay, hey I get drunk, wake up, I'm wasted still",
+    desc: '#빨간맛궁금해허니 콘텐츠를 선택한 당신! 가끔은 어른들의 아슬아슬하고 콘텐츠를 연애가 궁금하지 않으세요? 오늘은 특별히 그런 당신을 위해 준비된 콘텐츠를 즐겨주세요! 물론, 아이들에겐 비밀입니다🤫',
   },
   {
     name: '#당신의기숙사는',
-    desc: "I do the same thing I told you that I never would I told you I changed, even when I knew I never could I know that I can't find nobody else as good as you I need you to stay, need you to stay, hey I get drunk, wake up, I'm wasted still",
+    desc: '#당신의기숙사는 콘텐츠를 원하는 당신! 혹시 프리벳가 4번지 계단 밑 벽장, 9와 4분의 3 승강장을 생각하면 가슴이 뛰시나요? 아니면 아직도 호그와트 입학을 꿈꾸시는지? 아니면 옷장 밖에 있는 놀라운 세계를 좋아하시나요? 그렇다면 당신을 위해 준비된 이런 콘텐츠는 어떠신가요?',
   },
   {
     name: '#미친상상력의비밀',
-    desc: "I do the same thing I told you that I never would I told you I changed, even when I knew I never could I know that I can't find nobody else as good as you I need you to stay, need you to stay, hey I get drunk, wake up, I'm wasted still",
+    desc: '#미친상상력의비밀 콘텐츠를 원하는 당신! 당신이 상상해보지 못한 무언가를 경험해보고 싶지 않으세요? 혹은 판타지 SF 같은걸 좋아하신다고요? 그렇다면 이런 콘텐츠는 어떠신가요?',
   },
   {
     name: '#잔혹한',
-    desc: "I do the same thing I told you that I never would I told you I changed, even when I knew I never could I know that I can't find nobody else as good as you I need you to stay, need you to stay, hey I get drunk, wake up, I'm wasted still",
+    desc: '#잔혹한 콘텐츠를 원하는 당신! 내가 직접 겪고 싶진 않지만, 때때로 사람이 경험하기 힘든 잔인한 사건이 궁금하지 않으세요? 가끔은 이런 폭력적이고 위험한 냄새가 나는 콘텐츠가 당신의 쌓인 스트레스를 날려줄 수도? ',
   },
   {
     name: '#심장마비오는',
-    desc: "I do the same thing I told you that I never would I told you I changed, even when I knew I never could I know that I can't find nobody else as good as you I need you to stay, need you to stay, hey I get drunk, wake up, I'm wasted still",
+    desc: '#심장마비오는 콘텐츠를 원하는 당신! 귀신? 초월적인 현상? 그런 거 안 믿는다고요? 남들은 무섭다고 해도 도통 뭐가 무섭다는지 이해가 안 간다고요? 다른 사람들보다 튼튼한 심장을 가진 당신을 위해 준비한 콘텐츠 여기 있습니다.',
   },
   {
     name: '#흥미진진한',
-    desc: "I do the same thing I told you that I never would I told you I changed, even when I knew I never could I know that I can't find nobody else as good as you I need you to stay, need you to stay, hey I get drunk, wake up, I'm wasted still",
+    desc: '#흥미진진한 콘텐츠를 원하는 당신! 뭔가 신나는 걸 원하지만, 막상 나가서 즐기기엔 귀찮나요? 그런 당신을 위해 집에서 즐길 수 있는 몰입감 있는 콘텐츠를 소개해드릴게요.',
   },
   {
     name: '#아이들과보기좋은',
-    desc: "I do the same thing I told you that I never would I told you I changed, even when I knew I never could I know that I can't find nobody else as good as you I need you to stay, need you to stay, hey I get drunk, wake up, I'm wasted still",
+    desc: '#아이들과보기좋은을 선택한 당신! 아이들과 함께 즐길 수 있는 콘텐츠를 원하시는군요. 오늘은 집에서 아이들과 함께 재미있는 애니메이션 한편 어떠신가요? ',
   },
 ];
 export default infoList;
